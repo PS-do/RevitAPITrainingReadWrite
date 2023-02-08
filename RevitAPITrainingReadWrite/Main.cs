750a5cbe2ee7ff672856be63db83a97bdb6d992f
Сергей Плюхин <psg.pgs@mail.ru>
2023-02-08 20:05:58 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingReadWrite/Main.cs
@@ -0,0 +1,41 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingReadWrite
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            string roomInfo=string.Empty;
+            var rooms = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Rooms)
+                .Cast<Room>()
+                .ToList();
+            foreach (Room room in rooms)
+            {
+                string roomName = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString();
+                roomInfo += $"{roomName}\t{room.Number}\t{room.Area}{Environment.NewLine}";
+            }
+            string desktopPath=Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string csvPatch = Path.Combine(desktopPath, "roomInfo.csv");
+            File.WriteAllText(csvPatch, roomInfo);
+
+            return Result.Succeeded;
+        }
+    }
+}
