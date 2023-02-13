b1ba70467c1b70d93d3c8693cc17d2449a55f21e
Сергей Плюхин <psg.pgs@mail.ru>
2023-02-13 22:17:58 +0300

    ed

--- RevitAPITrainingReadWrite/Main.cs
+++ RevitAPITrainingReadWrite/Main.cs
@@ -2,6 +2,7 @@
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
+using Newtonsoft.Json;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 using System;
@@ -27,67 +28,23 @@ namespace RevitAPITrainingReadWrite
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
 
-
-
-            OpenFileDialog openFileDialog1 = new OpenFileDialog 
-            {
-                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-                Filter = "Excel files(*.xlsx) | *.xlsx" 
-            };
-
-            string filePath =string.Empty;
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                filePath = openFileDialog1.FileName;
-            }
-
-            if (string.IsNullOrEmpty(filePath))
-                return Result.Cancelled;
-
-            var rooms =new FilteredElementCollector(doc)
+            var rooms = new FilteredElementCollector(doc)
             .OfCategory(BuiltInCategory.OST_Rooms)
             .Cast<Room>()
             .ToList();
 
-            using(FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            var roomDataList = new List<RoomData>();
+            foreach (var room in rooms)
             {
-                IWorkbook workbook = new XSSFWorkbook(filePath); // создаём переменную для чтения файла экселя по указанному пути
-                ISheet sheet = workbook.GetSheetAt(index:0); // берём лист из которого будем читать данные. В данном случае - самый первый в файле.
-
-                // построчно проходим по листу и собираем данные
-                int rowIndex = 0;
-                while (sheet.GetRow(rowIndex)!=null)
+                roomDataList.Add(new RoomData
                 {
-                    if (sheet.GetRow(rowIndex).GetCell(0) == null ||
-                        sheet.GetRow(rowIndex).GetCell(1) == null)
-                    {
-                        rowIndex++;
-                        continue;
-                    }
-                    string name = sheet.GetRow(rowIndex).GetCell(0).StringCellValue;
-                    string number= sheet.GetRow(rowIndex).GetCell(1).StringCellValue;
-
-                    var room = rooms.FirstOrDefault(r => r.Number.Equals(number));// берём первое помещение из модели, у которого номер совпадает с тем номером,
-                                                                           // который находится в текущей строке
-
-                    if (room == null)
-                    {
-                        rowIndex++;
-                        continue;
-                    }
-
-                    using (var ts = new Transaction(doc, "Set parameter"))
-                    {
-                        ts.Start();
-                        room.get_Parameter(BuiltInParameter.ROOM_NAME).Set(name); 
-                        ts.Commit();
-
-                    }
-
-                    rowIndex++;
-                }
-
+                    Name = room.Name,
+                    Number = room.Number
+                });
             }
+
+            string json = JsonConvert.SerializeObject(roomDataList,Formatting.Indented);
+            File.WriteAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "data.json"),json);
 
             return Result.Succeeded;
         }
