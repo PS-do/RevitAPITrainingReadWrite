d56d4bf107c7e45d3ef75dc850bd47e85acdfbce
Сергей Плюхин <psg.pgs@mail.ru>
2023-02-13 21:16:21 +0300

    ed

--- RevitAPITrainingReadWrite/Main.cs
+++ RevitAPITrainingReadWrite/Main.cs
@@ -40,10 +40,55 @@ namespace RevitAPITrainingReadWrite
             {
                 filePath = openFileDialog1.FileName;
             }
+
+            if (string.IsNullOrEmpty(filePath))
+                return Result.Cancelled;
+
             var rooms =new FilteredElementCollector(doc)
             .OfCategory(BuiltInCategory.OST_Rooms)
             .Cast<Room>()
             .ToList();
+
+            using(FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                IWorkbook workbook = new XSSFWorkbook(filePath); // создаём переменную для чтения файла экселя по указанному пути
+                ISheet sheet = workbook.GetSheetAt(index:0); // берём лист из которого будем читать данные. В данном случае - самый первый в файле.
+
+                // построчно проходим по листу и собираем данные
+                int rowIndex = 0;
+                while (sheet.GetRow(rowIndex)!=null)
+                {
+                    if (sheet.GetRow(rowIndex).GetCell(0) == null ||
+                        sheet.GetRow(rowIndex).GetCell(1) == null)
+                    {
+                        rowIndex++;
+                        continue;
+                    }
+                    string name = sheet.GetRow(rowIndex).GetCell(0).StringCellValue;
+                    string number= sheet.GetRow(rowIndex).GetCell(1).StringCellValue;
+
+                    var room = rooms.FirstOrDefault(r => r.Equals(number));// берём первое помещение из модели, у которого номер совпадает с тем номером,
+                                                                           // который находится в текущей строке
+
+                    if (room == null)
+                    {
+                        rowIndex++;
+                        continue;
+                    }
+
+                    using (var ts = new Transaction(doc, "Set parameter"))
+                    {
+                        ts.Start();
+                        room.get_Parameter(BuiltInParameter.ROOM_NUMBER).Set(name); 
+                        ts.Commit();
+
+                    }
+
+                    rowIndex++;
+                }
+
+            }
+
             return Result.Succeeded;
         }
     }
